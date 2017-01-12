269f25be265f9dd6b25c37ee715dd86c1353091c
Brian Heller <bheller2010@live.com>
2017-01-12 15:47:11 -0800

    Fix unit test issue. For future reference, the pod has to be added to the test portion of the podfile.

--- Example/BHResting/AppDelegate.swift
+++ Example/BHResting/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import BHResting
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        BHRestManager.shared().setBaseUri(uri: "http://localhost:3000/")
+        print("Set base URI: \(BHRestManager.shared().baseUri()!)")
         return true
     }
 
